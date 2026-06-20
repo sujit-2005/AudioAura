c7bbac8e36c68d20329cca9c5036f8984d945895
sujit-2005 <venkatsujit.chinthala@gmail.com>
2026-06-21 01:58:20 +0530

    Delete hero-video.js

--- hero-video.js
+++
@@ -1,8 +0,0 @@
-const introVideo = document.getElementById("introVideo");
-const loopVideo = document.getElementById("loopVideo");
-
-introVideo.addEventListener("ended", () => {
-    introVideo.style.opacity = "0";
-    loopVideo.classList.remove("hidden");
-    loopVideo.play();
-});
